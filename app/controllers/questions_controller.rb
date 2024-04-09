fc315b9bdadfaa9bbff36b27fbbc3c499b13b3cb
Daniiltyhtin <daniiltyuhtin@yandex.com>
2024-04-09 22:24:44 +0300

    доработки

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,60 +1,56 @@
 class QuestionsController < ApplicationController
+  before_action :set_question!, only: %i[show edit update destroy]
 
-before_action :set_question, only: %i[ show edit update destroy]
+  def edit
+  end
 
-    def edit
+  def show
+    @answer = @question.answers.build
+  end
+
+  def update
+    if @question.update question_params
+      flash[:success] = 'Question updated'
+      redirect_to question_path
+    else
+      render :edit
     end
+  end
 
-    def show
-        @answers = @question.answers.build
+  def destroy
+    if @question.destroy
+      flash[:success] = 'Question deleted'
+      redirect_to questions_path
+    else
+      redirect_to questions_path
     end
+  end
 
+  def index
+    @questions = Question.all
+  end
 
+  def new
+    @question = Question.new
+  end
 
-    def update
-        if @question.update question_params
-            flash[:success] = "Question updated"
-            redirect_to question_path
-        else
-          render :edit
-        end
+  def create
+    @question = Question.new question_params
+    if @question.save
+      flash[:success] = 'Question created'
+      redirect_to questions_path
+    else
+      render :new
     end
+  end
 
-    def destroy
-        if @question.destroy
-            flash[:success] = "Question deleted"
-            redirect_to questions_path
-        else
-            redirect_to questions_path
-        end
-    end
+  private
 
-    def index
-        @questions = Question.all
-    end
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 
-    def new
-        @question = Question.new
-    end
-
-    def create
-        @question = Question.new question_params
-        if @question.save
-            flash[:success] = "Question created"
-            redirect_to questions_path
-        else
-            render :new
-        end
-    end
-
-    private
-
-    def question_params
-        params.require(:question).permit(:title, :body)
-    end
-
-    def set_question
-      @question = Question.find(params[:id])
-    end
-
+  def set_question!
+    @question = Question.find(params[:id])
+  end
 end
